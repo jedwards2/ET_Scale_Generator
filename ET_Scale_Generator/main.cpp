e390ef461d9f98a71700852e18f4d1c265f84da1
Jack <jack.morgan.edwards@gmail.com>
2022-10-04 14:49:19 -0400

    initial commit

--- ET_Scale_Generator/main.cpp
+++ ET_Scale_Generator/main.cpp
@@ -1,14 +1,6 @@
-//
-//  main.cpp
-//  ET_Scale_Generator
-//
-//  Created by Jack Edwards on 10/4/22.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  std::cout << "Hello, World!\n";
+  
   return 0;
 }
